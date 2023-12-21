eca18fb055ed282a1cda19fdc39b913e16162358
Frico Simon <simonfriko@gmail.com>
2023-12-21 17:42:18 +0700

    implement cors and modify the response for route /api/v1/checkout

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 9000;
+const cors = require('cors');
 
 const tokenizer = require('./controller/midtransTokenizer.js');
+
+app.use(cors());
 
 // accept json and url encoded values
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/v1/checkout', (req, res) => {
-    tokenizer();
-    res.send('checkout done');
+app.post('/api/v1/checkout', async (req, res) => {
+    const result = await tokenizer();
+    const token = result.token;
+    console.log("transaction token: ", token);
+    res.json({ token: token });
 })
 
 app.listen(port, () => {
