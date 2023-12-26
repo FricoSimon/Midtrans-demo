ef5c621e9384bb0474b0f1c9397ee173f7b2ab6d
Frico Simon <simonfriko@gmail.com>
2023-12-26 11:55:41 +0700

    create function to generate payment link

--- backend/controller/midtransTokenizer.js
+++ backend/controller/midtransTokenizer.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const axios = require('axios');
-const url = "https://app.sandbox.midtrans.com/snap/v1/transactions"
+const url = "https://app.sandbox.midtrans.com/snap/v1/transactions";
+const url_payment_link = "https://api.sandbox.midtrans.com/v1/payment-links";
 
 async function tokenizer(price) {
     const randomOrderId = Math.floor(Math.random() * 1000);
@@ -25,7 +26,45 @@ async function tokenizer(price) {
         return response.data
     } catch (error) {
         console.error(error);
+        throw error;
     }
 }
 
-module.exports = tokenizer;
+async function paymentLink(price) {
+    const randomOrderId = Math.floor(Math.random() * 1000);
+    let data = {
+        item_details: [
+            {
+                id: randomOrderId,
+                name: "Midtrans Pillow",
+                price: price,
+                quantity: 1,
+            }
+        ],
+        transaction_details: {
+            order_id: "ORDER-" + randomOrderId,
+            gross_amount: price
+        },
+    };
+
+    try {
+        const response = await axios({
+            url: url_payment_link,
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization:
+                    "Basic " +
+                    Buffer.from(process.env.SERVER_KEY_SECRET).toString("base64")
+            },
+            data: data
+        });
+        return response.data
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = { tokenizer, paymentLink };
