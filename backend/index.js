cab503723f44932cb889a71c516046063c5ad3ce
Frico Simon <simonfriko@gmail.com>
2023-12-26 11:55:30 +0700

    add route for generating payment link

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 9000;
 const cors = require('cors');
 
-const tokenizer = require('./controller/midtransTokenizer.js');
+const { tokenizer, paymentLink } = require('./controller/midtransTokenizer.js');
 
 app.use(cors());
 
@@ -17,6 +17,14 @@ app.post('/api/v1/checkout', async (req, res) => {
     const token = result.token;
     console.log("transaction token: ", token);
     res.json({ token: token });
+})
+
+app.post('/api/v1/payment-links', async (req, res) => {
+    const price = req.body.price;
+    const result = await paymentLink(price);
+    const payment_url = result.payment_url;
+    console.log("transaction token: ", payment_url);
+    res.json({ payment_url: payment_url });
 })
 
 app.listen(port, () => {
